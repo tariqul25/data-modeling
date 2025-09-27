196a818aea1a6ff5563b81a7dcb4e883dbc92066
tariqul26 <tariqulwebdev@gmail.com>
2025-09-28 03:28:38 +0600

    data modeling api add for routing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { type Application, type Request, type Response } from "express";
 import cors from 'cors';
 import { CourseRoutes } from "./app/models/course/course.route.js";
+import { MentorRoutes } from "./app/models/mentor/mentor.route.js";
+import { StudentRoutes } from "./app/models/student/student.route.js";
+import { ReviewRoutes } from "./app/models/review/review.route.js";
 
 import { MentorRoutes } from "./app/models/mentor/mentor.route.js";
 import { StudentRoutes } from "./app/models/student/student.route.js";
