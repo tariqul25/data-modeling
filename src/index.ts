d86c6c7bed498bb0c0f9638939514a237ef3884c
tariqul26 <tariqulwebdev@gmail.com>
2025-09-28 03:28:38 +0600

    data modeling api add for routing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express, { type Application, type Request, type Response } from "express";
 import cors from 'cors';
 import { CourseRoutes } from "./app/models/course/course.route.js";
+import { MentorRoutes } from "./app/models/mentor/mentor.route.js";
+import { StudentRoutes } from "./app/models/student/student.route.js";
+import { ReviewRoutes } from "./app/models/review/review.route.js";
 
 const app: Application = express()
 
 app.use(express.json());
 app.use(cors());
 
-app.use("api/coursers", CourseRoutes)
+app.use("api/courses", CourseRoutes)
+app.use("api/mentors", MentorRoutes)
+app.use("api/students", StudentRoutes)
+app.use("api/reviews", ReviewRoutes)
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
